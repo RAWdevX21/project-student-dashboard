842687909f3fded807a583419a8e53050ab3047b
RicDW <rawdevx21@gmail.com>
2023-08-08 02:56:59 -0400

    structured folders and uploaded student profiles

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import StudentBody from './Features/StudentBody/index.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" alt="Vite Logo Link">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" alt="React Logo Link">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Student Dashboard</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <StudentBody />
     </>
   )
 }
